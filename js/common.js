298356ffbf7337729ff7ed1037f1430331df1b2a
Yevgen <godun.yevgen@gmail.com>
2013-05-17 15:41:11 +0300

    new sidebar menu!

--- js/common.js
+++ js/common.js
@@ -1,3 +1,22 @@
 $(document).ready(function() {
+
+// none transition effects
     $.mobile.defaultPageTransition = 'none';
+
+// Show/hide menu sidebar
+    $(".btn-menu").click(function(){
+        if ($(this).hasClass("js-active")) {
+            $(".btn-menu").removeClass("js-active");
+            $("body").removeClass("is-with-sidebar");
+        }
+        else {
+            $(".btn-menu").addClass("js-active");
+            $("body").addClass("is-with-sidebar");
+        }
+        
+    });
+    $(".js-overlay").click(function(){
+        $("body").removeClass("is-with-sidebar");
+        $(".btn-menu").removeClass("js-active");
+    });
 });
